0bd00bc1fee4f22ebd9584f71115c15d4c4fb178
Mikael Pajari <mikael.pajari@live.se>
2020-09-17 12:14:21 +0200

    klar

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace e8kwva
+{
+    class Program
+    { 
+        enum players
+                {
+                    P1 = 1,
+                    P2 = 4,
+                }   
+        static void Main(string[] args)
+        {
+           
+            int[,]Playground = new int [3, 3]
+            { // spelplanen 
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            };
+                
+                
+                bool Gameactive = true;
+                players activeplayer = players.P1;
+            
+            
+            
+            
+            
+
+               
+
+           
+           
+            
+            while(Gameactive)
+            {
+                
+               
+                
+               
+
+            }
+            
+
+            
+
+
+           
+        }
+    }
+}
